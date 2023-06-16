07c78d163ad86fddd28694b7fbca4414f5346a6d
Radenz <radenrifqirahman@gmail.com>
2023-06-16 10:00:36 +0700

    fix: add tailwind nesting plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const tailwindcss = require('tailwindcss');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const autoprefixer = require('autoprefixer');
 
 const config = {
-  plugins: [
+  plugins: {
+    'tailwindcss/nesting': {},
     //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    tailwindcss(),
+    // tailwindcss(),
+    tailwindcss,
     //But others, like autoprefixer, need to run after,
     autoprefixer
-  ]
+  }
 };
 
 module.exports = config;
