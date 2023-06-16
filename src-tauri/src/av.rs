908de82c23da844efaef2e12b8843bac0d30ad7f
Radenz <radenrifqirahman@gmail.com>
2023-06-16 09:18:35 +0700

    refactor: use let-else syntax for early return on error

--- src-tauri/src/av.rs
+++ src-tauri/src/av.rs
@@ -59,15 +59,14 @@ pub enum StreamKind {
 
 #[tauri::command]
 pub fn discover_streams(path: String) -> Streams {
-    let context = ffmpeg::format::input(&path);
-    if let Err(_) = context {
+    let maybe_input = ffmpeg::format::input(&path);
+    let Ok(input) = maybe_input else {
         return Streams::new();
-    }
+    };
 
-    let mut context = context.unwrap();
     let mut streams = Streams::new();
 
-    for stream in context.streams() {
+    for stream in input.streams() {
         let meta = stream.meta();
         match stream.kind() {
             StreamKind::Video => streams.add_video(meta),
@@ -87,11 +86,10 @@ trait StreamExt {
 
 impl StreamExt for Stream<'_> {
     fn kind(&self) -> StreamKind {
-        let codec = Context::from_parameters(self.parameters());
-        if let Err(_) = codec {
+        let maybe_codec = Context::from_parameters(self.parameters());
+        let Ok(codec) = maybe_codec else {
             return StreamKind::Unknown;
-        }
-        let codec = codec.unwrap();
+        };
         match codec.medium() {
             media::Type::Video => StreamKind::Video,
             media::Type::Audio => StreamKind::Audio,
@@ -106,78 +104,3 @@ impl StreamExt for Stream<'_> {
         StreamMeta::new(id, index)
     }
 }
-
-// fn main() {
-//     ffmpeg::init().unwrap();
-
-//     match ffmpeg::format::input(&"out.mp4".to_owned()) {
-//         Ok(mut context) => {
-//             let mut subtitle_stream_index = -1;
-//             let mut decoder = None;
-
-//             for stream in context.streams() {
-//                 let codec = ffmpeg::codec::context::Context::from_parameters(stream.parameters())
-//                     .expect("invalid codec");
-
-//                 if codec.medium() == media::Type::Subtitle {
-//                     println!("Got subtitle stream with index: {}", stream.index());
-//                     let time_base = stream.time_base();
-//                     println!("With time base of {}/{}", time_base.0, time_base.1);
-//                     subtitle_stream_index = stream.index() as i32;
-//                     decoder = codec.decoder().subtitle().ok();
-
-//                     let meta = stream.metadata();
-//                     println!("Stream metadata");
-//                     for (key, value) in meta.iter() {
-//                         println!("{key}: {value}");
-//                     }
-//                 }
-//             }
-
-//             if subtitle_stream_index < 0 {
-//                 return;
-//             }
-
-//             let mut decoder = decoder.unwrap();
-//             // let mut data = vec![];
-
-//             let mut subtitle = Subtitle::new();
-
-//             for (stream, mut packet) in context.packets() {
-//                 if stream.index() == subtitle_stream_index as usize {
-//                     decoder.decode(&packet, &mut subtitle);
-//                     for rect in subtitle.rects() {
-//                         match rect {
-//                             Rect::Ass(ass_text) => println!("Got ASS: {}", ass_text.get()),
-//                             Rect::Text(text) => println!("Got TEXT: {}", text.get()),
-//                             Rect::Bitmap(bitmap) => println!("Got BITMAP: {}", bitmap.width()),
-//                             _ => (),
-//                         }
-//                     }
-//                     let pts = packet.pts().expect("No pts found");
-//                     let duration = packet.duration();
-//                     println!(
-//                         "Which should be presented at {} with duration of {}",
-//                         pts, duration
-//                     );
-//                 }
-//             }
-
-//             // println!("{:?}", subtitle.start());
-
-//             // unsafe {
-//             // let mut frame = Frame::empty();
-//             // while let Ok(_) = decoder.receive_frame(&mut frame) {
-//             // println!("{:?}", frame.metadata());
-//             // }
-//             // }
-
-//             // let chunk = packet.data().unwrap_or(&[]);
-//             // data.extend_from_slice(chunk);
-
-//             // let subtitle = String::from_utf8(data).expect("Decoding error");
-//             // fs::write("out.txt", subtitle);
-//         }
-//         Err(err) => (),
-//     }
-// }
